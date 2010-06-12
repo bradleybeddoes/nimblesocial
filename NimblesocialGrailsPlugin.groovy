0f2d2458b36d9c560e64698171739e5a488cc441
Michael Kimsal <Michael Kimsal>
2010-06-12 16:59:49 -0400

    Needs quotes around dependency value

--- NimblesocialGrailsPlugin.groovy
+++ NimblesocialGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class NimblesocialGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2-M3 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [nimble:0.4-SNAPSHOT > *]
+    def dependsOn = [nimble:"0.4-SNAPSHOT > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
